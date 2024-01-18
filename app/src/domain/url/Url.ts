87a9441ffaeea0e0dd7f059d8f7167a4884c4fc1
sadegh <s.s.marashi@gmail.com>
2024-01-18 12:16:12 +0330

    add getter , implemet url data mapper and implement shorten it

--- app/src/domain/url/Url.ts
+++ app/src/domain/url/Url.ts
@@ -1,28 +1,57 @@
 export class Url {
-    private static normalizerModule = null;  
+    private static normalizerModule = null;
 
     constructor(
-        private guid: string,
         private short: string,
         private long: string,
         private lastVisitedAt: Date,
         private visitCount: number,
-        private userGuid: string,
+        private userId: string,
+        private id: string = ""
     ) { }
 
     getShort(): string {
         return this.short;
     }
 
+    getLong(): string {
+        return this.long;
+    }
+
+    getLastVisitedAt(): Date {
+        return this.lastVisitedAt;
+    }
+
+    getVisitCount(): number {
+        return this.visitCount;
+    }
+
+    getUserId(): string {
+        return this.userId;
+    }
+
+    getId(): string {
+        return this.id;
+    }
+
     public static async normalize(long: string): Promise<string | null> {
         if (Url.normalizerModule === null) {
             try {
                 Url.normalizerModule = await import("normalize-url");
-            } catch(error) {
+            } catch (error) {
                 return null;
             }
         }
 
         return Url.normalizerModule['default'](long);
+    }
+
+    public static async shortenIt(short: string, long: string, userId: string): Promise<Url | null> {
+        const normalizedLong = await Url.normalize(long);
+        if (normalizedLong === null) {
+            return null;
+        }
+
+        return new Url(short, normalizedLong, new Date(), 0, userId);
     }
 }
--- app/src/infrastructure/url/UrlDataMapper.ts
+++ app/src/infrastructure/url/UrlDataMapper.ts
@@ -1,15 +1,35 @@
 import { injectable } from "inversify";
 import { DataMapper } from "../../domain/DataMapper";
 import { Url } from "../../domain/url/Url";
+import { ObjectId } from "mongodb";
 
 @injectable()
 export class UrlDataMapper implements DataMapper<Url>
 {
     toDomain(dalEntity: any): Url {
-        return null;
+        return new Url(
+            dalEntity.short,
+            dalEntity.long,
+            dalEntity.lastVisitedAt,
+            dalEntity.visitCount,
+            dalEntity.userId.toString(),
+            dalEntity._id.toString(),
+        );
     }
 
     toDalEntity(url: Url): any {
-        return null;
+        const dal = {
+            short: url.getShort(),
+            long: url.getLong(),
+            lastVisitedAt: url.getLastVisitedAt(),
+            visitCont: url.getVisitCount(),
+            userId: ObjectId.createFromHexString(url.getUserId()),
+        };
+
+        if (url.getId() !== "") {
+            dal["_id"] = ObjectId.createFromHexString(url.getId());
+        }
+
+        return dal;
     }
 }
