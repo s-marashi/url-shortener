21b4cbbe0add240ad6dad0028170106704ad4cca
sadegh <s.s.marashi@gmail.com>
2024-01-19 23:06:43 +0330

    combining subscribe and cron logic using promise all

---
+++ housekeeper-app/src/index.ts
@@ -0,0 +1,37 @@
+import { asyncContainer } from "./container";
+import { Container, inject } from "inversify";
+import "reflect-metadata";
+import { TYPES } from "./TYPES";
+import { CronScheduleHandler } from "./infrastructure/CronScheduleHandler";
+import { UrlResolvedQueue } from "./domain/urlResolved/UrlResolvedQueue";
+
+(async () => {
+  const container = new Container();
+  await container.loadAsync(asyncContainer);
+  const results:any[] = await Promise.all([
+    // Subscribe Promise
+    new Promise(async (resolve, reject)=>{
+      try {        
+        const queue: UrlResolvedQueue = container.get<UrlResolvedQueue>(TYPES.UrlResolvedQueue);
+        await queue.subscribe(container.get(TYPES.ResolveTrackApplication));
+        resolve({name: "subscribe"});
+      } catch(error) {
+        reject(error.toString());
+      }
+    }),
+    // Cron Promise
+    new Promise((resolve, reject)=>{
+      try {
+        const cron: CronScheduleHandler = new CronScheduleHandler();
+        cron.schedule("*/5 * * * * *", container.get(TYPES.UnusedUrlCleanupApplication));
+        resolve({name: "cron"});
+      } catch (error) {
+        reject(error.toString());
+      }
+    }),
+    // Http Promise
+    new Promise((resolve, reject)=>{}),
+  ]);
+
+  console.log(results);
+})();
