7d3f20cfd6d006a96a3c7a198a4fe2dadc3f96ea
sadegh <s.s.marashi@gmail.com>
2024-01-20 14:58:15 +0330

    add health check to house keeper

--- housekeeper-app/src/index.ts
+++ housekeeper-app/src/index.ts
@@ -4,34 +4,36 @@ import "reflect-metadata";
 import { TYPES } from "./TYPES";
 import { CronScheduleHandler } from "./infrastructure/CronScheduleHandler";
 import { UrlResolvedQueue } from "./domain/urlResolved/UrlResolvedQueue";
+import { InversifyExpressServer } from "inversify-express-utils";
+import { Express as ExpressApplication, json } from 'express';
+import cors from 'cors';
+import './controllers/HealthCheckController';
 
 (async () => {
   const container = new Container();
   await container.loadAsync(asyncContainer);
-  const results: any[] = await Promise.all([
-    // Subscribe Promise
-    new Promise(async (resolve, reject) => {
-      try {
-        const queue: UrlResolvedQueue = container.get<UrlResolvedQueue>(TYPES.UrlResolvedQueue);
-        await queue.subscribe(container.get(TYPES.ResolveTrackApplication));
-        resolve({ name: "subscribe" });
-      } catch (error) {
-        reject(error.toString());
-      }
-    }),
-    // Cron Promise
-    new Promise((resolve, reject) => {
-      try {
-        const cron: CronScheduleHandler = new CronScheduleHandler();
-        cron.schedule("* */12 * * *", container.get(TYPES.UnusedUrlCleanupApplication));
-        resolve({ name: "cron" });
-      } catch (error) {
-        reject(error.toString());
-      }
-    }),
-    // Http Promise
-    new Promise((resolve, reject) => { }),
-  ]);
 
-  console.log(results);
+  // API Server initialisation
+  const server = new InversifyExpressServer(container);
+  server.setConfig((app: ExpressApplication) => {
+    app.use(cors());
+    app.use(json());
+  });
+
+  // server.setErrorConfig((app: ExpressApplication) => {
+  //   app.use((err, req, res, next) => {
+  //     console.log(err, req);
+  //     next();
+  //   });
+  // });
+
+  server.build().listen(6000, () => {
+    console.log("Server started on http://localhost:6000");
+  });
+
+  const queue: UrlResolvedQueue = container.get<UrlResolvedQueue>(TYPES.UrlResolvedQueue);
+  await queue.subscribe(container.get(TYPES.ResolveTrackApplication));
+
+  const cron: CronScheduleHandler = new CronScheduleHandler();
+  cron.schedule("* */12 * * *", container.get(TYPES.UnusedUrlCleanupApplication));
 })();
